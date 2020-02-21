24663ede66c021287599b0868819b78d13f8aa74
Nitesh <niteshsastra2005@gmail.com>
2020-02-21 19:56:05 +0530

    Update allClipboardText.py

--- allClipboardText.py
+++ allClipboardText.py
@@ -1,11 +1,14 @@
 import pyperclip
 
+lineNumber=1
 oldClip=""
 while(1==1):
     clip = pyperclip.paste()
     if(oldClip != clip):
+        print("-------------------------------------- Clip Item % 2d ---------------------------------" %(lineNumber))
         print(clip)
         oldClip = clip
+        lineNumber = lineNumber + 1
     if(clip.lower() == 'exit'):
         break
- 
+        
